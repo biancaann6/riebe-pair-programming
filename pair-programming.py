699d6ae15980a83401b29ccb99e355f5a7c5e062
Jude Lowe <jlowe2@mines.edu>
2024-02-26 22:26:23 -0700

    add feedback and test function

--- pair-programming.py
+++ pair-programming.py
@@ -6,3 +6,12 @@ def offset_mean(array, target_mean):
     array_mean = np.mean(array)         # Find mean of original array
     offset = target_mean - array_mean   # Difference between target and original mean
     return array + offset               # Return the new array that has the new mean
+
+#Looks good
+
+
+#Test the function
+
+def test():
+    print(offset_mean([2, 4, 5, 1, 2], 5))
+    print(offset_mean([1, 2], 7))
